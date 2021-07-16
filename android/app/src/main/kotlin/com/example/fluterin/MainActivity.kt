0deff1c2d82fc579ce18aeadbe9a6bebefbba698
Jesuar David López <jesuar.lopez@farsiman.com>
2021-07-16 00:59:46 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/fluterin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluterin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,5 @@
+import 'package:fluterin/screens/details_screen.dart';
+export 'package:fluterin/screens/home_screen.dart';
+export 'package:fluterin/screens/details_screen.dart'; 
+
+
